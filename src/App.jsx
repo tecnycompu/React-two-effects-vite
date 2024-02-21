b40438746aabcd7cae3abe38ea5870e5df451e2d
tecnycompu <tecnycompu@gmail.com>
2024-02-21 08:21:54 -0500

    UPDATE, IMAGEN CAMBIA AL PASAR EL MOUSE

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [catFact, setCatFact] = useState(''); // Use more descriptive name
+  const [catFact, setCatFact] = useState('');
   const [catImageUrl, setCatImageUrl] = useState('');
 
   useEffect(() => {
@@ -27,8 +27,8 @@ function App() {
           const response = await fetch(`https://cataas.com/cat/says/${firstWord}`);
 
           if (response.ok) {
-            const data = await response.blob(); // Handle data as blob
-            const imageUrl = URL.createObjectURL(data); // Generate URL from blob
+            const data = await response.blob();
+            const imageUrl = URL.createObjectURL(data);
             setCatImageUrl(imageUrl);
           } else {
             console.error('Error fetching cat image:', response.statusText);
@@ -42,10 +42,26 @@ function App() {
     }
   }, [catFact]);
 
+  const handleMouseEnter = () => {
+    setCatFact('');
+    setCatImageUrl('');
+    fetchCatFact(); // Llamar a la función para obtener un nuevo hecho y una nueva imagen de gato.
+  };
+
+  const fetchCatFact = async () => {
+    try {
+      const response = await fetch('https://catfact.ninja/fact');
+      const data = await response.json();
+      setCatFact(data.fact);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h1>Random Cat Fact</h1>
-      {catFact && <p>{catFact}</p>}
+      {catFact && <p onMouseEnter={handleMouseEnter}>{catFact}</p>}
       {catImageUrl && (
         <img src={catImageUrl} alt="random cat" width={300} height={300} />
       )}
